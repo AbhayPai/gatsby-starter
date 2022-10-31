d0744e4532f2177e06aa8aa669ffb470b8da0fd3
Abhay Pai <pai.abhay8@gmail.com>
2022-10-31 17:43:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Gatsby with lando
+
+1. `lando start`
+2. `lando gatsby build`
+2. `lando gatsby develop`
+
---
+++ gatsby-node.js
@@ -0,0 +1,30 @@
+exports.onCreateWebpackConfig = ({ getConfig, stage, actions }) => {
+  landoReload(getConfig, stage, actions);
+}
+
+function landoReload(getConfig, stage, actions) {
+  if (stage !== "develop") {
+    return;
+  }
+
+  if (typeof process.env.LANDO === 'undefined') {
+    return;
+  }
+
+  process.env.PUBLIC_HOST = 'gatsby-starter.lndo.site';
+  let config = getConfig();
+
+  if (typeof process.env.PUBLIC_HOST !== 'undefined' && typeof config.output !== 'undefined') {
+    config.output.publicPath = '';
+    config.entry.commons = config.entry.commons.map(point => {
+      if (/webpack-hot-middleware/.test(point)) {
+        point += '&dynamicPublicPath=true'
+      }
+      return point;
+    });
+  }
+
+  actions.setWebpackConfig({
+    output: config.output, entry: config.entry, devServer: config.devServer
+  });
+}
